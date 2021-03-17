3cb8c3486efc0c806b4745bcb73726b4e0a5dc7c
Thiago Rodrigues <thiagor_@live.com>
2021-03-17 01:25:02 -0300

    feat: all pass

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,22 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { name, email } = request.body;
+
+      const user = this.createUserUseCase.execute({
+        name,
+        email,
+      });
+
+      if (!user) {
+        return response.status(400).send();
+      }
+
+      return response.status(201).json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists || !userExists.admin) {
+      throw new Error("bad request");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
